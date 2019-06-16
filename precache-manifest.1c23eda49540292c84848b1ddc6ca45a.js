b6ef218b380ddb9110ef8680b6dcd1ab9196aa5c
DoinaToderici <doina.toderici@gmail.com>
2019-06-16 14:08:12 +0200

    Updates

---
+++ precache-manifest.1c23eda49540292c84848b1ddc6ca45a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd17f523b3c25cb79ab671b42df3306e",
+    "url": "/site_under_construction/index.html"
+  },
+  {
+    "revision": "fa43ac95a0290dd45af7",
+    "url": "/site_under_construction/static/css/main.c8b0ae6d.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/site_under_construction/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "fa43ac95a0290dd45af7",
+    "url": "/site_under_construction/static/js/main.738ebb8c.chunk.js"
+  },
+  {
+    "revision": "42eedfd570c4bac92f4c",
+    "url": "/site_under_construction/static/js/runtime~main.23d4c8aa.js"
+  }
+]);
